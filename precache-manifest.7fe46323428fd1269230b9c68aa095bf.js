3c242b41776cc9ddd65155d026ee5f6f0d471074
aubehub <albalopsanchez@gmail.com>
2020-07-20 01:32:30 -0300

    Updates

---
+++ precache-manifest.7fe46323428fd1269230b9c68aa095bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633ddb7eccde6c98c0d3a15409d8a038",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "27e86a98e314fda6e078",
+    "url": "/meme-generator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "24f10fd4fa4f8e519b87",
+    "url": "/meme-generator/static/css/main.d1201a86.chunk.css"
+  },
+  {
+    "revision": "27e86a98e314fda6e078",
+    "url": "/meme-generator/static/js/2.e07df736.chunk.js"
+  },
+  {
+    "revision": "73092e34f706fcdc5f2b21c97238d7dc",
+    "url": "/meme-generator/static/js/2.e07df736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f10fd4fa4f8e519b87",
+    "url": "/meme-generator/static/js/main.8e00f9bf.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
